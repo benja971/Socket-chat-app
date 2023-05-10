766d6ca6a453302d3adb3d776f749e2780215259
Benjamin NIDDAM <nddm.benjamin@gmail.com>
2023-05-10 20:46:55 +0200

    `create user` route

--- src/routes/users.router.ts
+++ src/routes/users.router.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
 import validateResource from '../middlewares/validateResources';
+import { createUserHandler, findUserByIdHandler, findUserByUsername, getUsersHandler } from '../controllers/user.controller';
+import { FindUserByIdSchema, createUserSchema, getUserSchema, getUsersSchema } from '../schemas/user.schema';
 const router = Router();
 
+router.post('/', validateResource(createUserSchema), createUserHandler);
 export default router;
