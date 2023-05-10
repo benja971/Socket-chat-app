03e338a1036a4483e757127f8528a95cb9cbcbd1
Benjamin NIDDAM <nddm.benjamin@gmail.com>
2023-05-10 20:35:27 +0200

    Basic routing

---
+++ src/routes/router.ts
@@ -0,0 +1,33 @@
+import express, { Router } from 'express';
+import { join } from 'path';
+import log from '../utils/logger';
+
+const router = Router();
+
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+
+router.use('/', express.static(join(__dirname, '..', '..', 'public')));
+
+router.use((req, _, next) => {
+	log.debug(`${req.method} ${req.path}`);
+	next();
+});
+
+router.use('/api/v1', router);
+
+
+router.get('*.css', (req, res) => {
+	res.sendFile(join(__dirname, '..', '..', 'public', 'styles', 'css', req.path));
+});
+
+router.get('*.js', (req, res) => {
+	if (req.path === '/socket.io.js') {
+		const path = join(__dirname, '..', '..', 'node_modules', 'socket.io', 'client-dist', 'socket.io.js');
+		res.sendFile(path);
+		return;
+	}
+	res.sendFile(join(__dirname, '..', '..', 'public', 'scripts', req.path));
+});
+
+export default router;
